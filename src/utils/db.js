57e1fce60692e125f63876d3519d4c5157050a43
Manoj <manoj@internshala.com>
2023-08-20 16:48:18 +0530

    minor fix

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,13 +1,18 @@
 const { Sequelize } = require("sequelize");
 
-const db = 'todo';
+const db = process.env.DBNAME ?? 'todo';
 const username = process.env.DBUSERNAME ?? 'admin';
 const password = process.env.DBPASSWORD ?? '12345678';
 const host = process.env.DBHOST ?? 'localhost';
 
 const sequelize = new Sequelize(db, username, password, {
     host: host,
-    dialect: process.env.DB ?? 'mysql'
+    dialect: process.env.DB ?? 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: 'true'
+        }
+    },
 });
 
 module.exports = sequelize;
